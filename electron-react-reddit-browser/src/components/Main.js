8a6c79f220901d89d93451e6d68efc1ed079f08f
Nathanael Fletcher <nathanaelfletcher19@gmail.com>
2020-04-13 13:51:53 -0400

    search works. thumbnail displays if thumbnail is valid, shows icon if not.

--- electron-react-reddit-browser/src/components/Main.js
+++ electron-react-reddit-browser/src/components/Main.js
@@ -1,4 +1,9 @@
 import React, {Component} from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+import Icon from '../icon.png'
 
 const electron = window.require('electron')
 const ipcRenderer = electron.ipcRenderer
@@ -8,18 +13,22 @@ class Main extends Component {
   constructor(props){
     super(props)
     this.state = {
-      posts: []
+      posts: [],
+      search: ''
     }
     this.getSub = this.getSub.bind(this)
     this.show = this.show.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSearch = this.handleSearch.bind(this)
   }
 
-  async getSub(){
+  async getSub(sub){
     try {
-      let response = await fetch(`https://www.reddit.com/r/aww.json?raw_json=1`)
+      let response = await fetch(`https://www.reddit.com/r/${sub}.json?raw_json=1`)
       let data = await response.json()
       this.setState({
-        posts: data.data.children
+        posts: data.data.children,
+        search: ''
       })
     } catch(error) {
       console.error(error);
@@ -30,13 +39,18 @@ class Main extends Component {
     this.setState({[e.target.id]: e.target.value})
   }
 
+  handleSearch(e){
+    e.preventDefault()
+    this.getSub(this.state.search)
+  }
+
   show(image) {
     console.log(image);
     ipcRenderer.send('toggle-show', image)
   }
 
   componentDidMount() {
-    this.getSub()
+    this.getSub('aww')
   }
 
 
@@ -44,15 +58,24 @@ class Main extends Component {
   render(){
     return(
       <div>
-        <h1>Electron Reddit</h1>
-        <ul className="list-group list-group-flush">
+        <Navbar className="bg-light justify-content-around">
+          <Form inline onSubmit={this.handleSearch}>
+            <FormControl type="text" placeholder="r/" className="mr-sm-2" value={this.state.search} id="search" onChange={this.handleChange}/>
+            <Button type='submit' variant="outline-warning" >Search</Button>
+          </Form>
+        </Navbar>
+        <ul className="new-list-group new-list-group-flush">
           {this.state.posts.map(post => (
             <li
               key={post.data.id}
-              className="list-group-item flex-container"
+              className="new-list-group-item new-flex-container"
               onClick={() => this.show(post.data.preview.images[0].source.url)}
               >
-              <img src={post.data.thumbnail} alt="thumbnail" className="thumbnail"/>
+              {post.data.thumbnail_height
+                ? <img src={post.data.thumbnail} alt="thumbnail" className="thumbnail"/>
+              : <img src={Icon} alt="thumbnail" className="thumbnail"/>
+              }
+
               <div>{post.data.title}</div>
             </li>
           ))}
--- electron-react-reddit-browser/src/index.js
+++ electron-react-reddit-browser/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import ViewManager from './ViewManager.js'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
