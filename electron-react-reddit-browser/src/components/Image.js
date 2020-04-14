54b03a2537f534ef535a0bda067727e9f213190c
Nathanael Fletcher <nathanaelfletcher19@gmail.com>
2020-04-13 23:31:05 -0400

    added spiny logo during loading

--- electron-react-reddit-browser/src/components/Image.js
+++ electron-react-reddit-browser/src/components/Image.js
@@ -12,13 +12,17 @@ class Image extends Component {
       permalink: '',
       op: {},
       opImg: false,
-      comments: []
+      comments: [],
+      loaded: false
     }
     this.getComments = this.getComments.bind(this)
     this.handleClose = this.handleClose.bind(this)
   }
 
   async getComments(link){
+    this.setState({
+      loaded: false
+    })
     try {
       let response = await fetch(`https://www.reddit.com${link}.json?raw_json=1`)
       let data = await response.json()
@@ -26,7 +30,8 @@ class Image extends Component {
       this.setState({
         op: data[0].data.children[0].data,
         opImg: data[0].data.children[0].data.preview,
-        comments: data[1].data.children
+        comments: data[1].data.children,
+        loaded: true
       })
     } catch(error) {
       console.error(error);
@@ -50,33 +55,42 @@ class Image extends Component {
 
   render(){
     return(
-      <div>
-          <Button onClick={this.handleClose} variant="light">X</Button>
-        <div className='container'>
-          <div className='title-div'>
-            <h3 className='show-title'>{this.state.op.title}</h3>
+      <>
+      {this.state.loaded
+        ? <div>
+            <Button onClick={this.handleClose} variant="light">X</Button>
+          <div className='container'>
+            <div className='title-div'>
+              <h3 className='show-title'>{this.state.op.title}</h3>
+            </div>
+            <div className='title-div'>
+              {this.state.opImg ? <img className="show-img" src={this.state.opImg.images[0].source.url}></img> : ''}
+            </div>
+            {this.state.op.selftext ? <p>{this.state.op.selftext}</p> : ''}
           </div>
-          <div className='title-div'>
-            {this.state.opImg ? <img className="show-img" src={this.state.opImg.images[0].source.url}></img> : ''}
+          <div className='comments'>
+            <ul className="new-list-group new-list-group-flush">
+              {this.state.comments.map(comment =>(
+                <li
+                  key={comment.data.id}
+                  className="new-list-group-item new-flex-container"
+                  >
+                    <div className="comment">
+                      <p className='author'>{comment.data.author}</p>
+                      <div>{comment.data.body}</div>
+                    </div>
+                  </li>
+              ))}
+            </ul>
           </div>
-          {this.state.op.selftext ? <p>{this.state.op.selftext}</p> : ''}
         </div>
-        <div className='comments'>
-          <ul className="new-list-group new-list-group-flush">
-            {this.state.comments.map(comment =>(
-              <li
-                key={comment.data.id}
-                className="new-list-group-item new-flex-container"
-                >
-                  <div className="comment">
-                    <p className='author'>{comment.data.author}</p>
-                    <div>{comment.data.body}</div>
-                  </div>
-                </li>
-            ))}
-          </ul>
+        :
+        <div className="title-div">
+          <img className="App-logo" src="../icon.png"></img>
         </div>
-      </div>
+      }
+
+      </>
     )
   }
 }
